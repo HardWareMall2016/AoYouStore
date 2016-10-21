9ff30d1c1a261d7d7b203e7fbeddff1785d8d020
keke <983198505@qq.com>
2016-10-21 22:45:10 +0800

    提交代码

--- app/src/main/java/com/zhan/aoyoustore/ui/fragment/mine/MineMain.java
+++ app/src/main/java/com/zhan/aoyoustore/ui/fragment/mine/MineMain.java
@@ -1,10 +1,17 @@
 package com.zhan.aoyoustore.ui.fragment.mine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.zhan.aoyoustore.R;
+import com.zhan.aoyoustore.base.UserInfo;
+import com.zhan.aoyoustore.ui.activity.MainActivity;
+import com.zhan.framework.support.inject.ViewInject;
 import com.zhan.framework.ui.fragment.ABaseFragment;
+import com.zhan.framework.utils.ToastUtils;
 
 /**
  * 作者：蒲柯柯 on 2016/10/21 16:55
@@ -13,6 +20,29 @@ import com.zhan.framework.ui.fragment.ABaseFragment;
  */
 public class MineMain extends ABaseFragment {
 
+    @ViewInject(id = R.id.tv_edit_person,click = "OnClick")
+    TextView mTvEditPerson;
+
+    @ViewInject(id = R.id.tv_Receipt_address,click = "OnClick")
+    TextView mTvReceiptAddress;
+
+    @ViewInject(id = R.id.tv_change_password,click = "OnClick")
+    TextView mTvChangePsw;
+
+    @ViewInject(id = R.id.tv_Order_management,click = "OnClick")
+    TextView mTvOrderManager;
+
+    @ViewInject(id = R.id.tv_Purchase_records,click = "OnClick")
+    TextView mTvPurchaseRecords;
+
+    @ViewInject(id = R.id.tv_Collection,click = "OnClick")
+    TextView mTvCollection;
+
+    @ViewInject(id = R.id.tv_contact,click = "OnClick")
+    TextView mTvContact;
+
+    @ViewInject(id = R.id.tv_quit,click = "OnClick")
+    TextView mTvQuit;
 
     //Tools
     private LayoutInflater mInflater;
@@ -29,5 +59,37 @@ public class MineMain extends ABaseFragment {
         getActivity().setTitle("会员中心");
     }
 
+    void OnClick(View view){
+        switch (view.getId()){
+            case R.id.tv_edit_person:
+                MinePersonFragment.launch(getActivity());
+                break;
+            case R.id.tv_Receipt_address:
+                MineReceiptAddressFragment.launch(getActivity());
+                break;
+            case R.id.tv_change_password:
+                MineChangePswFragment.launch(getActivity());
+                break;
+            case R.id.tv_Order_management:
+                MineOrderManagementFragment.launch(getActivity());
+                break;
+            case R.id.tv_Purchase_records:
+                PurchaseRecordFragment.launch(getActivity());
+                break;
+            case R.id.tv_Collection:
+                MineCollectionFragment.launch(getActivity());
+                break;
+            case R.id.tv_contact:
+                MineContactFragment.launch(getActivity());
+                break;
+            case R.id.tv_quit:
+                UserInfo.logout();
+                ToastUtils.toast("退出登录");
+                Intent homePageIntent = new Intent(getActivity(), MainActivity.class);
+                homePageIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homePageIntent);
+                break;
+        }
+    }
 
 }
