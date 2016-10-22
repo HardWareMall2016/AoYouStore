75d65a921b2432aea952e9e8acb7a04292391191
keke <983198505@qq.com>
2016-10-22 14:05:57 +0800

    提交

--- app/src/main/java/com/zhan/aoyoustore/ui/fragment/mine/MineMain.java
+++ app/src/main/java/com/zhan/aoyoustore/ui/fragment/mine/MineMain.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.zhan.aoyoustore.R;
@@ -43,6 +46,23 @@ public class MineMain extends ABaseFragment {
 
     @ViewInject(id = R.id.tv_quit,click = "OnClick")
     TextView mTvQuit;
+
+    @ViewInject(id = R.id.rl_account,click = "OnClick")
+    RelativeLayout mRlAccount;
+    @ViewInject(id = R.id.ll_account)
+    LinearLayout mLlAcount;
+    @ViewInject(id = R.id.iv_account_arrow)
+    ImageView mIVAccountArrow;
+
+    @ViewInject(id = R.id.rl_order,click = "OnClick")
+    RelativeLayout mRlOrder;
+    @ViewInject(id = R.id.ll_order_manager)
+    LinearLayout mLlOrderMagager;
+    @ViewInject(id = R.id.iv_order_arrow)
+    ImageView mIvOrderArrow;
+
+    private boolean AccountFlag = true;
+    private boolean OrederFlag = true;
 
     //Tools
     private LayoutInflater mInflater;
@@ -88,6 +108,28 @@ public class MineMain extends ABaseFragment {
                 Intent homePageIntent = new Intent(getActivity(), MainActivity.class);
                 homePageIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(homePageIntent);
+                break;
+            case R.id.rl_account:
+                if (AccountFlag){
+                    mLlAcount.setVisibility(View.VISIBLE);
+                    AccountFlag = false;
+                    mIVAccountArrow.setImageResource(R.drawable.arrow_up_small);
+                }else{
+                    mLlAcount.setVisibility(View.GONE);
+                    AccountFlag = true;
+                    mIVAccountArrow.setImageResource(R.drawable.arrow_right_small);
+                }
+                break;
+            case R.id.rl_order:
+                if(OrederFlag){
+                    mLlOrderMagager.setVisibility(View.VISIBLE);
+                    OrederFlag = false;
+                    mIvOrderArrow.setImageResource(R.drawable.arrow_up_small);
+                }else {
+                    mLlOrderMagager.setVisibility(View.GONE);
+                    OrederFlag = true;
+                    mIvOrderArrow.setImageResource(R.drawable.arrow_right_small);
+                }
                 break;
         }
     }
